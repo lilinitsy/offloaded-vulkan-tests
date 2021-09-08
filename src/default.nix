255161e826a5d021ebe51ff8cdd4360b8b4558f8
Nathan Ringo <nathan@remexre.xyz>
2021-09-07 19:02:36 -0500

    Misc nix changes.

---
+++ src/default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.stdenv.mkDerivation {
+  name = "offloaded-vulkan-tests";
+  src = pkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ] ./.;
+  buildInputs = with pkgs; [ glfw glm pkg-config vulkan-loader ];
+  makeFlags = [ "-f" "Makefile_nix" ];
+  installPhase = "install -Dt $out client rendertest";
+}
