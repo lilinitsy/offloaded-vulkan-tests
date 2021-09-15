af9a454e54a573680356b9404313c2e8cdf2b0f4
Nathan Ringo <nathan@remexre.xyz>
2021-09-15 03:02:42 -0500

    Update Nix build script and .gitignore.

--- src/default.nix
+++ src/default.nix
@@ -3,7 +3,7 @@
 pkgs.stdenv.mkDerivation {
   name = "offloaded-vulkan-tests";
   src = pkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ] ./.;
-  buildInputs = with pkgs; [ glfw glm pkg-config vulkan-loader ];
+  buildInputs = with pkgs; [ coz glfw glm pkg-config vulkan-loader ];
   makeFlags = [ "-f" "Makefile_nix" ];
   installPhase = "install -Dt $out client rendertest";
 }
