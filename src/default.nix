8492367abf2c0778928e9d8eb84c89c4b508df63
Nathan Ringo <nathan@remexre.xyz>
2021-12-09 19:35:59 -0600

    Nix build updates for 2.4

--- src/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.stdenv.mkDerivation {
-  name = "offloaded-vulkan-tests";
-  src = pkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ] ./.;
-  buildInputs = with pkgs; [ coz glfw glm pkg-config vulkan-loader ];
-  makeFlags = [ "-f" "Makefile_nix" ];
-  installPhase = "install -Dt $out client rendertest";
-}
