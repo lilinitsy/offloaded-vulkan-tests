e4471844323288c2d286e827371b473110ef3696
Nathan Ringo <nathan@remexre.xyz>
2022-01-07 14:51:50 -0600

    unformatting

--- obj-loader/include/obj_loader.h
+++ obj-loader/include/obj_loader.h
@@ -5,53 +5,62 @@
 #include <stdlib.h>
 
 #ifdef __cplusplus
-namespace obj {
-extern "C" {
+namespace obj
+{
+	extern "C"
+	{
 #endif
 
-struct F32s {
-  const float *ptr;
-  size_t len;
-};
+		struct F32s
+		{
+			const float *ptr;
+			size_t len;
+		};
 
-struct U32s {
-  const uint32_t *ptr;
-  size_t len;
-};
+		struct U32s
+		{
+			const uint32_t *ptr;
+			size_t len;
+		};
 
-struct Model {
-  F32s positions;
-  F32s normals;
-  F32s texcoords;
-  U32s indices;
-  size_t material_id;
-  uint8_t has_material;
-  const char *name;
-};
+		struct Model
+		{
+			struct F32s positions;
+			struct F32s normals;
+			struct F32s texcoords;
+			struct U32s indices;
+			size_t material_id;
+			uint8_t has_material;
+			const char *name;
+		};
 
-struct Models {
-  const struct Model *ptr;
-  size_t len;
-};
+		struct Models
+		{
+			const struct Model *ptr;
+			size_t len;
+		};
 
-struct Material {
-  const char *diffuse_texture;
-};
+		struct Material
+		{
+			const char *diffuse_texture;
+		};
 
-struct Materials {
-  const struct Material *ptr;
-  size_t len;
-};
+		struct Materials
+		{
+			const struct Material *ptr;
+			size_t len;
+		};
 
-struct ModelsAndMaterials {
-  Models models;
-  Materials materials;
-};
+		struct ModelsAndMaterials
+		{
+			struct Models models;
+			struct Materials materials;
+		};
 
-struct ModelsAndMaterials load_obj(const char *path);
+		struct ModelsAndMaterials load_obj(const char *path);
 
 #ifdef __cplusplus
-}
+	}
 }
 #endif
 
